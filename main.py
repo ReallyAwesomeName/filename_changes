b989214deb769933fd95f00c30cebf7a3a2393b4
Jason <jg832@njit.edu>
2021-12-28 14:59:43 -0500

    Recieved instructions, now using filename object

--- main.py
+++ main.py
@@ -1,62 +1,103 @@
 # clean up verbose filenames - replace with simple ones with only pertinent info
 
+"""
+Instructions:
+
+- Write a program to convert the returns from the format in the first picture, to the 
+format of the second picture.
+- Some returns will have indications as to what their tax jurisdiction is like the 
+NJ returns below in the second picture, in this case, I need to manually look at 
+the PDF and confirm the jurisdiction. (I will include further instruction on this).
+
+What I need to keep:
+
+-State abbreviation most important
+-What the PDF is (EDI Confirmation, Return, Return Payment)
+-The year and month (202111) Keep in mind this will change month to month.
+-Keep all the letters and put them in the front. Example: The first PDF in picture 1.
+Should code to: "NC_ST_SU_EDI Confirmation_202111"
+- There are going to be returns that have multiple returns for its state. If you see 
+in the first picture, there are a few from Ohio (OH). To combat this issue, look at 
+the beginning numbers in picture 1. There are 5 PDFs with 4860802, that means it's 
+all one return. For states with multiple returns, including keeping all the letters, 
+please link those together. What do I mean by link together? For those same 5 pdf 
+examples, do like the above format and add "(1)." In this case, this return has 
+two confirmations, you can do "1a" & "1b".
+-If you can't program some of the above tasks, it's okay. Whatever you can do is
+amazing.
+
+What I need to delete:
+
+- All the numbers and special characters.
+- Match it as best you can to picture 2 but keep the ST_SU as well because it'll make 
+both our lives easier and in future if anything you or I could edit the code down 
+the road.
+"""
+
 import os
 
 
-state_abbreviations = [
-    "AL",
-    "AK",
-    "AZ",
-    "AR",
-    "CA",
-    "CO",
-    "CT",
-    "DE",
-    "FL",
-    "GA",
-    "HI",
-    "ID",
-    "IL",
-    "IN",
-    "IA",
-    "KS",
-    "KY",
-    "LA",
-    "ME",
-    "MD",
-    "MA",
-    "MI",
-    "MN",
-    "MS",
-    "MO",
-    "MT",
-    "NE",
-    "NV",
-    "NH",
-    "NJ",
-    "NM",
-    "NY",
-    "NC",
-    "ND",
-    "OH",
-    "OK",
-    "OR",
-    "PA",
-    "RI",
-    "SC",
-    "SD",
-    "TN",
-    "TX",
-    "UT",
-    "VT",
-    "VA",
-    "WA",
-    "WV",
-    "WI",
-    "WY",
-]
-
 codes = ["TT", "KB", "FB", "GG"]
+
+
+class filename:
+    """filename object with attributes: identifier (4860802), sub_identifier ((1)),
+    type (EDI Confirmation, Return, Return Payment), date (202111),
+    state_code (NC_ST_SU),
+    """
+
+    state_abbreviations = [
+        "AL",
+        "AK",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "FL",
+        "GA",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VA",
+        "WA",
+        "WV",
+        "WI",
+        "WY",
+    ]
 
 
 def main():
@@ -90,11 +131,11 @@ def change_name(ufile):
     # remove .pdf, add back before return
     ufile_noext = ufile.strip(".pdf")
     # TODO: check for other delimiters
-    ufile_split = ufile_noext.split("_")
+    ufile_split = ufile_noext.split("~")
 
     for item in ufile_split:
         # check states
-        if item in state_abbreviations:
+        if item in filename.state_abbreviations:
             new_name.append(item)
         # check codes
         if item in codes:
